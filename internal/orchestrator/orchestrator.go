e45653c140645680ec4dbb96f643a94327e27cdd
Alessio Perugini <alessio@perugini.xyz>
2025-07-11 09:01:00 +0200

    bump bricks to 0.1.7

--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -65,7 +65,7 @@ const (
 
 func init() {
 	const dockerRegistry = "ghcr.io/bcmi-labs/"
-	const dockerPythonImage = "arduino/appslab-python-apps-base:0.1.6"
+	const dockerPythonImage = "arduino/appslab-python-apps-base:0.1.7"
 	// Registry base: contains the registry and namespace, common to all Arduino docker images.
 	registryBase := os.Getenv("DOCKER_REGISTRY_BASE")
 	if registryBase == "" {
