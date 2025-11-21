60a2f797d49c1fbdd4a00ef7a1ed36fa88e2725c
Giulio Pilotto <pilotto.giulio@gmail.com>
2025-11-21 15:20:22 +0100

    rename to RequireModel

--- internal/orchestrator/bricks/types.go
+++ internal/orchestrator/bricks/types.go
@@ -20,14 +20,14 @@ type BrickListResult struct {
 }
 
 type BrickListItem struct {
-	ID            string   `json:"id"`
-	Name          string   `json:"name"`
-	Author        string   `json:"author"`
-	Description   string   `json:"description"`
-	Category      string   `json:"category"`
-	Status        string   `json:"status"`
-	ModelRequired bool     `json:"require_model"`
-	Models        []string `json:"models"`
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	Author       string   `json:"author"`
+	Description  string   `json:"description"`
+	Category     string   `json:"category"`
+	Status       string   `json:"status"`
+	RequireModel bool     `json:"require_model"`
+	Models       []string `json:"models"`
 }
 
 type AppBrickInstancesResult struct {
@@ -42,7 +42,7 @@ type BrickInstance struct {
 	Status           string                `json:"status"`
 	Variables        map[string]string     `json:"variables,omitempty" description:"Deprecated: use config_variables instead. This field is kept for backward compatibility."`
 	ConfigVariables  []BrickConfigVariable `json:"config_variables,omitempty"`
-	ModelRequired    bool                  `json:"require_model,omitempty"`
+	RequireModel     bool                  `json:"require_model"`
 	ModelID          string                `json:"model,omitempty"`
 	CompatibleModels []AIModel             `json:"compatible_models"`
 }
@@ -81,7 +81,7 @@ type BrickDetailsResult struct {
 	Description      string                   `json:"description"`
 	Category         string                   `json:"category"`
 	Status           string                   `json:"status"`
-	ModelRequired    bool                     `json:"require_model"`
+	RequireModel     bool                     `json:"require_model"`
 	Variables        map[string]BrickVariable `json:"variables,omitempty"`
 	Readme           string                   `json:"readme"`
 	ApiDocsPath      string                   `json:"api_docs_path"`
