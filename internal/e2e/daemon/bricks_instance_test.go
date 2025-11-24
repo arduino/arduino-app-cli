2690465eef5b09cf66bce9b605d37a6930df3f99
mirkoCrobu <m.crobu@ext.arduino.cc>
2025-11-24 09:21:45 +0100

    fix test e2e

--- internal/e2e/daemon/bricks_instance_test.go
+++ internal/e2e/daemon/bricks_instance_test.go
@@ -146,9 +146,9 @@ func TestGetAppBrickInstanceById(t *testing.T) {
 		require.NotEmpty(t, brickInstance.JSON200)
 		require.Equal(t, ImageClassifactionBrickID, *brickInstance.JSON200.Id)
 		require.Equal(t, expectedConfigVariables, (*brickInstance.JSON200.ConfigVariables))
-		require.Nil(t, brickInstance.JSON200.CompatibleModels)
+		require.NotNil(t, brickInstance.JSON200.CompatibleModels)
+		require.Equal(t, expectedModelInfo, *(brickInstance.JSON200.CompatibleModels))
 	})
-
 	t.Run("GetAppBrickInstanceByBrickIDWithCompatibleModels_Success", func(t *testing.T) {
 		brickInstance, err := httpClient.GetAppBrickInstanceByBrickIDWithResponse(
 			t.Context(),
@@ -159,7 +159,7 @@ func TestGetAppBrickInstanceById(t *testing.T) {
 		require.NotEmpty(t, brickInstance.JSON200)
 		require.Equal(t, ImageClassifactionBrickID, *brickInstance.JSON200.Id)
 		require.NotNil(t, brickInstance.JSON200.CompatibleModels)
-		require.Equal(t, expectedModelInfo, *(brickInstance.JSON200.CompatibleModels))
+		require.NotEqual(t, expectedModelInfo, *(brickInstance.JSON200.CompatibleModels))
 	})
 
 	t.Run("GetAppBrickInstanceByBrickID_InvalidAppID_Fails", func(t *testing.T) {
