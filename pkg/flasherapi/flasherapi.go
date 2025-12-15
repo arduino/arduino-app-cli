a23f45bad42f9f70018d0c80b763ce10ce68a8c3
mirkoCrobu <m.crobu@ext.arduino.cc>
2025-12-15 15:02:24 +0100

    refactoring

--- pkg/flasherapi/flasherapi.go
+++ pkg/flasherapi/flasherapi.go
@@ -1,3 +1,18 @@
+// This file is part of arduino-app-cli.
+//
+// Copyright 2025 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-app-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
 package flasherapi
 
 import (
@@ -8,22 +23,23 @@ import (
 	"github.com/arduino/arduino-app-cli/pkg/board/remote"
 )
 
+const R0_IMAGE_VERSION_ID = "20250807-136"
+
 // GetOSImageVersion returns the version of the OS image used in the board.
 // It is used by the AppLab to enforce image version compatibility.
 func GetOSImageVersion(ctx context.Context, conn remote.RemoteConn) (string, error) {
-	const defaultVersion = "20250807-136"
 
-	output, err := conn.GetCmd("cat /etc/buildinfo").Output(ctx)
+	output, err := conn.GetCmd("cat", "/etc/buildinfo").Output(ctx)
 	if err != nil {
-		return defaultVersion, err
+		return R0_IMAGE_VERSION_ID, err
 	}
 
 	if version, ok := ParseOSImageVersion(string(output)); ok {
 		slog.Info("find OS Image version", "version", version)
 		return version, nil
 	}
-	slog.Info("Unable to find OS Image version", "using default version", defaultVersion)
-	return defaultVersion, nil
+	slog.Info("Unable to find OS Image version", "using default version", R0_IMAGE_VERSION_ID)
+	return R0_IMAGE_VERSION_ID, nil
 }
 
 func ParseOSImageVersion(buildInfo string) (string, bool) {
@@ -41,78 +57,4 @@ func ParseOSImageVersion(buildInfo string) (string, bool) {
 		}
 	}
 	return "", false
-}
-
-type OSImageRelease struct {
-	VersionLabel string
-	ID           string
-	Latest       bool
-}
-
-func ListAvailableOSImages() []OSImageRelease {
-	return []OSImageRelease{
-		{
-			ID:           "20251123-159",
-			VersionLabel: "r159 (2025-11-23)",
-			Latest:       true,
-		},
-	}
-}
-
-func IsUserPartitionPreservationSupported(conn remote.RemoteConn, targetImageVersion OSImageRelease) bool {
-	// targetImageVersion is the version of the image to be flashed
-	// some older versions do not support user partition preservation
-	// so this has to be considered here
-	return true
-}
-
-type FlashStep string
-
-const (
-	FlashStepPrecheck    FlashStep = "precheck"
-	FlashStepDetection   FlashStep = "detection"
-	FlashStepDownloading FlashStep = "downloading"
-	FlashStepExtracting  FlashStep = "extracting"
-	FlashStepFlashing    FlashStep = "flashing"
-)
-
-type FlashEvent struct {
-	Step            FlashStep
-	OverallProgress int    // percentage 0-100
-	Message         string // log message to show on the UI
-}
-
-func Flash(
-	ctx context.Context, // context to cancel to interrupt the flashing process
-	// conn remote.RemoteConn, // is this required for the flash process?
-	imageVersion OSImageRelease, // OS image version to flash
-	preserveUserPartition bool, // whether to preserve the user partition or not
-	eventCB func(event FlashEvent), // Callback, sends progress events to the caller
-) error {
-	// The disk space check is done before starting the flashing process.
-	return InsufficientDiskSpaceError{
-		RequiredSpaceMB:  2048,
-		AvailableSpaceMB: 1024,
-	}
-}
-
-// Errors returned by the Flash function above.
-// Assertions can be done on the caller side to show better error messages.
-
-type QDLFlashError struct {
-	Details string
-	Cause   error
-}
-
-type DownloadError struct {
-	Details string
-}
-
-type InsufficientDiskSpaceError struct {
-	RequiredSpaceMB  int64
-	AvailableSpaceMB int64
-}
-
-func (e InsufficientDiskSpaceError) Error() string {
-	return "insufficient disk space"
 }
