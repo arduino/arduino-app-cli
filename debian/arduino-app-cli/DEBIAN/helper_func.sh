7192278cfb707657269e119066cb8fe6bcc7d603
Marta Carbone <m.carbone@arduino.cc>
2025-12-24 15:15:03 +0100

    Address review.

--- debian/arduino-app-cli/DEBIAN/helper_func.sh
+++ debian/arduino-app-cli/DEBIAN/helper_func.sh
@@ -13,16 +13,28 @@ cleanup_arduino_examples() {
         local EXAMPLES=$(find "${EXAMPLES_DIR}" -maxdepth 1 -mindepth 1 -type d 2>/dev/null)
         echo "Stopping apps and clearing cache in: ${EXAMPLES_DIR}"
         for dir_path in ${EXAMPLES}; do
-            # 1. Stop the application (suppress output and errors)
+            # Stop apps and cache cleanup
             sudo -u arduino /usr/bin/arduino-app-cli app stop "${dir_path}" > /dev/null 2>&1 || true
-            
-            # 2. Remove the cache directory
             local CACHE_PATH="${dir_path}/.cache"
-            
-            # Check if the cache directory exists before attempting to remove it
             if [ -d "${CACHE_PATH}" ]; then
                 rm -r "${CACHE_PATH}"
             fi
         done
+    fi
+}
+
+# Function: copy if exists
+# Description: Checks for the existence of the source path before attempting to copy.
+#
+# Arguments:
+#   $1 - Source
+#   $2 - Destination
+#
+copy_if_exists() {
+    local SRC="$1"
+    local DST="$2"
+
+    if [ -d "${SRC}" ]; then
+        cp -r "${SRC}" "${DST}"
     fi
 }
