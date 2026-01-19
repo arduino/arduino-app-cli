82f9657628ec25d0759ce6638cb547598d593968
Giulio Pilotto <pilotto.giulio@gmail.com>
2026-01-19 17:56:32 +0100

    test model install

--- internal/orchestrator/modelsindex/edgeimpulse_test.go
+++ internal/orchestrator/modelsindex/edgeimpulse_test.go
@@ -1,6 +1,9 @@
 package modelsindex
 
 import (
+	"context"
+	"log"
+	"os"
 	"testing"
 
 	paths "github.com/arduino/go-paths-helper"
@@ -29,4 +32,56 @@ func TestListEdgeImpulseModels(t *testing.T) {
 		ModelConfiguration: map[string]string{"EI_OBJ_DETECTION_MODEL": "testdata/ei-models/111111/1/my-model.eim"},
 	}
 	assert.Equal(t, expectedModel, eimodels[0], "loaded model does not match expected model")
+}
+
+// TODO should be mocked and add a e2e test with an Arduino organization
+func TestInstallEIModel(t *testing.T) {
+
+	customEIModelsDir := paths.New(os.Getenv("ARDUINO_CUSTOM_MODEL_PATH"))
+	ApiKey := os.Getenv("ARDUINO_EI_API_KEY")
+	ApiUrl := os.Getenv("ARDUINO_EI_API_URL")
+
+	if err := os.MkdirAll(customEIModelsDir.String(), 0o755); err != nil {
+		log.Fatalf("failed to create directory %s: %v", customEIModelsDir, err)
+	}
+
+	eiClient := NewEIClient(ApiKey, ApiUrl)
+	ProjectID := 876194
+	ImpulseID := 1
+
+	err := InstallEIModel(context.TODO(), eiClient, *customEIModelsDir, ProjectID, ImpulseID)
+	if err != nil {
+		log.Fatalf("failed to install EI model: %v", err)
+	}
+
+	model, err := SaveEIModel(context.TODO(), eiClient, *customEIModelsDir, ProjectID, ImpulseID)
+	if err != nil {
+		log.Fatalf("failed to save EI model metadata: %v", err)
+	}
+
+	models, err := LoadEdgeImpulseModels(customEIModelsDir)
+	if err != nil {
+		log.Fatalf("failed to load EI models: %v", err)
+	}
+
+	if found, ok := func() (*AIModel, bool) {
+		for _, m := range models {
+			if m.ID == model.ID {
+				return &m, true
+			}
+		}
+		return nil, false
+	}(); !ok {
+		log.Fatalf("installed model with ID %s not found in loaded models", model.ID)
+	} else {
+		assert.Equal(t, model.Name, found.Name, "model name does not match")
+		assert.Equal(t, model.ModuleDescription, found.ModuleDescription, "model description does not match")
+		assert.Equal(t, model.Runner, found.Runner, "model runner does not match")
+		assert.Equal(t, model.Bricks, found.Bricks, "model bricks do not match")
+		assert.Equal(t, model.Metadata, found.Metadata, "model metadata does not match")
+	}
+
+	if err := os.RemoveAll(customEIModelsDir.String()); err != nil {
+		log.Fatalf("failed to delete %s: %v", customEIModelsDir, err)
+	}
 }
