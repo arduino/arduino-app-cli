4a3e84286f0c2f872028b1cb6d377fa4c915713d
Cristian Maglie <c.maglie@bug.st>
2026-01-19 10:21:49 +0100

    Removed useless json tags

--- internal/orchestrator/sketch_libs_release_id.go
+++ internal/orchestrator/sketch_libs_release_id.go
@@ -26,9 +26,9 @@ import (
 // - name[@version]
 // Version is optional, if not provided, the latest version available will be used.
 type LibraryReleaseID struct {
-	Name         string `json:"name"`
-	Version      string `json:"version"`
-	IsDependency bool   `json:"is_dependency"`
+	Name         string
+	Version      string
+	IsDependency bool
 }
 
 func NewLibraryReleaseID(name string, version string) LibraryReleaseID {
