492fbabc7957994b7e27ab8f89e96e62e63f85ca
dido18 <davideneri18@gmail.com>
2026-01-23 10:49:17 +0100

    revert space

--- internal/orchestrator/bricks/bricks.go
+++ internal/orchestrator/bricks/bricks.go
@@ -80,6 +80,7 @@ func (s *Service) AppBrickInstancesList(a *app.ArduinoApp) (AppBrickInstancesRes
 		}
 
 		variablesMap, configVariables := getInstanceBrickConfigVariableDetails(brick, brickInstance.Variables)
+
 		res.BrickInstances[i] = BrickInstance{
 			ID:              brick.ID,
 			Name:            brick.Name,
@@ -90,7 +91,6 @@ func (s *Service) AppBrickInstancesList(a *app.ArduinoApp) (AppBrickInstancesRes
 			ModelID:         cmp.Or(brickInstance.Model, brick.ModelName),
 			Variables:       variablesMap,
 			ConfigVariables: configVariables,
-
 			CompatibleModels: f.Map(s.modelsIndex.GetModelsByBrick(brick.ID), func(m modelsindex.AIModel) AIModel {
 				return AIModel{
 					ID:          m.ID,
