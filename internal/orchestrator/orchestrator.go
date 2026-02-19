fd2f10d0862f0f59f5aa5dcf938884819247d535
Marta Carbone <m.carbone@arduino.cc>
2026-02-19 14:09:16 +0100

    Snapshot.

--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -32,6 +32,7 @@ import (
 	"github.com/arduino/arduino-cli/commands"
 	rpc "github.com/arduino/arduino-cli/rpc/cc/arduino/cli/commands/v1"
 	"github.com/arduino/go-paths-helper"
+	"github.com/compose-spec/compose-go/v2/validation"
 	"github.com/docker/cli/cli/command"
 	"github.com/goccy/go-yaml"
 	"github.com/gosimple/slug"
