dba1958a7fbc9d4571fa8051e62c7c50bff2c563
dido18 <davideneri18@gmail.com>
2026-03-31 15:09:19 +0200

    fix: update bricks folder path in TestLoadBrickYamlBrickIndex

--- internal/orchestrator/bricksindex/bricks_index_test.go
+++ internal/orchestrator/bricksindex/bricks_index_test.go
@@ -304,7 +304,7 @@ func TestLoadBrickYamlBrickIndex(t *testing.T) {
 	t.Run("load yaml index and local folders", func(t *testing.T) {
 		bricksIndex, err := Load(paths.New("testdata/0.4.8"))
 		require.NoError(t, err)
-		brickWithLoca := bricksIndex.WithBricksFolder(paths.New("testdata"))
+		brickWithLoca := bricksIndex.WithBricksFolder(paths.New("testdata/bricks"))
 		brick, found := brickWithLoca.FindBrickByID("my-first-brick")
 		require.True(t, found)
 		require.Equal(t, "my-first-brick", brick.ID)
