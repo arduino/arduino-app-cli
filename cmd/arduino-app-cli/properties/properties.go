791ef9b0d92cd4771c762e1a859c56c902ff67c3
dido18 <davideneri18@gmail.com>
2026-03-31 13:16:51 +0200

    remove last part fro each filede

--- cmd/arduino-app-cli/properties/properties.go
+++ cmd/arduino-app-cli/properties/properties.go
@@ -14,12 +14,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
-// You can be released from the requirements of the above licenses by purchasing
-// a commercial license. Buying such a license is mandatory if you want to
-// modify or otherwise use the software for commercial activities involving the
-// Arduino software without disclosing the source code of your own applications.
-// To purchase a commercial license, send an email to license@arduino.cc.
 
 package properties
 
