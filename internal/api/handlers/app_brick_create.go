7e3901ba0f9acb44f5c41d38b9eeb301ea634561
dido18 <davideneri18@gmail.com>
2026-04-03 14:35:41 +0200

    refactor: remove description parameter from local brick creation

--- internal/api/handlers/app_brick_create.go
+++ internal/api/handlers/app_brick_create.go
@@ -72,7 +72,7 @@ func HandleAppLocalBrickCreate(idProvider *app.IDProvider) http.HandlerFunc {
 			return
 		}
 
-		err = generator.GenerateLocalBrick(a.GetBricksPath(), id, req.Name, req.Description)
+		err = generator.GenerateLocalBrick(a.GetBricksPath(), id, req.Name)
 		if err != nil {
 			if errors.Is(err, generator.ErrBrickAlreadyExists) {
 				render.EncodeResponse(w, http.StatusConflict, models.ErrorResponse{Details: fmt.Sprintf("a brick with the same id '%s' already exists", id)})
