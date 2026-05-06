45d34daafee72c39f78e82a2661d7c4369fe04e8
dido18 <davideneri18@gmail.com>
2026-05-06 09:31:28 +0200

    fix: update command in pullDockerImages to use 'system init'

--- internal/update/apt/service.go
+++ internal/update/apt/service.go
@@ -240,7 +240,7 @@ func runAptCleanCommand(ctx context.Context) iter.Seq2[string, error] {
 
 func pullDockerImages(ctx context.Context) iter.Seq2[string, error] {
 	return func(yield func(string, error) bool) {
-		cmd, err := paths.NewProcess(nil, "arduino-app-cli", "version")
+		cmd, err := paths.NewProcess(nil, "arduino-app-cli", "system", "init")
 		if err != nil {
 			_ = yield("", err)
 			return
