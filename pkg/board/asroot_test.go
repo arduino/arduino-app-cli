ce56ee026c3541e4c72ff8cad2084e4c670058f6
lucarin91 <l.rinaldi@arduino.cc>
2026-05-19 18:10:38 +0200

    chore(pkg/board): add test for asroot

---
+++ pkg/board/asroot_test.go
@@ -0,0 +1,52 @@
+package board_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/arduino/arduino-app-cli/internal/testtools"
+	"github.com/arduino/arduino-app-cli/pkg/board"
+	"github.com/arduino/arduino-app-cli/pkg/board/remote"
+	"github.com/arduino/arduino-app-cli/pkg/board/remote/adb"
+	"github.com/arduino/arduino-app-cli/pkg/board/remote/ssh"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecAsRoot(t *testing.T) {
+	name, adbPort, sshPort := testtools.StartAdbDContainer(t)
+	t.Cleanup(func() { testtools.StopAdbDContainer(t, name) })
+
+	conns := []struct {
+		name     string
+		conn     remote.RemoteConn
+		password string
+	}{
+		{
+			name: "adb",
+			conn: func() remote.RemoteConn {
+				conn, err := adb.FromHost("localhost:"+adbPort, "")
+				require.NoError(t, err)
+				return conn
+			}(),
+			password: "arduino",
+		},
+		{
+			name: "ssh",
+			conn: func() remote.RemoteConn {
+				conn, err := ssh.FromHost("arduino", "arduino", "127.0.0.1:"+sshPort)
+				require.NoError(t, err)
+				return conn
+			}(),
+			password: "arduino",
+		},
+	}
+
+	for _, tc := range conns {
+		t.Run(tc.name, func(t *testing.T) {
+			out, err := board.ExecAsRoot(tc.conn, tc.password, "whoami")
+			require.NoError(t, err)
+			assert.True(t, strings.HasPrefix(string(out), "root"))
+		})
+	}
+}
