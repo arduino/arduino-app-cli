982b410930e6403c8585e4516d14db39f15d3b63
Marta Carbone <m.carbone@arduino.cc>
2026-05-26 07:49:56 +0200

    Fix discussion items

--- cmd/arduino-app-cli/internal/servicelocator/servicelocator.go
+++ cmd/arduino-app-cli/internal/servicelocator/servicelocator.go
@@ -94,4 +94,9 @@ var (
 	GetPlatform = sync.OnceValue(func() platform.Platform {
 		return platform.GetPlatform(globalConfig.DataDir())
 	})
+
+	// // TODO
+	// NpuInit = sync.OnceValue(func() platform.Platform {
+
+	// })
 )
