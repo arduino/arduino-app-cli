d3c823e9407405f843c9bb844a9078af96a7b2d7
dido18 <davideneri18@gmail.com>
2026-07-07 14:00:01 +0200

    refactor: rename isModelRequired to isAiBrick for clarity

--- internal/orchestrator/bricksindex/bricks_index.go
+++ internal/orchestrator/bricksindex/bricks_index.go
@@ -220,7 +220,7 @@ func (b Brick) GetModelNameByBoard(platform platform.Platform) string {
 	return defaultModelName
 }
 
-func (b Brick) isModelRequired(platform platform.Platform) bool {
+func (b Brick) isAiBrick(platform platform.Platform) bool {
 	for _, mb := range b.ModelByBoard {
 		if mb.Platform == platform.BoardName && mb.Model != "" {
 			return true
@@ -295,7 +295,7 @@ func Load(platform platform.Platform, path *paths.Path) (*BricksIndex, error) {
 		yamlIndex.Bricks[i].ReadmeFile = path.Join("docs", namespace, brickName, "README.md")
 		yamlIndex.Bricks[i].ExamplesPath = path.Join("examples", namespace, brickName)
 		yamlIndex.Bricks[i].DocsAPIPath = path.Join("api-docs", namespace, "app_bricks", brickName, "API.md")
-		yamlIndex.Bricks[i].RequireModel = yamlIndex.Bricks[i].isModelRequired(platform)
+		yamlIndex.Bricks[i].RequireModel = yamlIndex.Bricks[i].isAiBrick(platform)
 
 		// Load main compose file and, if present, platform-specific compose files
 		var (
