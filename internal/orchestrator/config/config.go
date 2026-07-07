2ecba6698a7b9fbeca7b55aa51721acbbb1cd266
mirkoCrobu <m.crobu@ext.arduino.cc>
2026-07-07 15:23:24 +0200

    re-generate assets

--- internal/orchestrator/config/config.go
+++ internal/orchestrator/config/config.go
@@ -22,7 +22,7 @@ import (
 )
 
 // runnerVersion do not edit, this is generate with `task generate:assets`
-var RunnerVersion = "0.11.0rc10"
+var RunnerVersion = "0.11.0rc11"
 
 type Configuration struct {
 	appsDir                          *paths.Path
