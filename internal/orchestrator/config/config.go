fad35f7185682a254dd559d7379914591e7fcb28
mirkoCrobu <m.crobu@ext.arduino.cc>
2026-07-30 14:54:38 +0200

    bump example to rc6

--- internal/orchestrator/config/config.go
+++ internal/orchestrator/config/config.go
@@ -22,7 +22,7 @@ import (
 )
 
 // runnerVersion do not edit, this is generate with `task bump:runner-version`
-var RunnerVersion = "0.12.0rc3"
+var RunnerVersion = "0.12.0rc6"
 
 type Configuration struct {
 	appsDir                          *paths.Path
