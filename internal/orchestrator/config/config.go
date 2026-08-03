05a7bc9ca0f91fd9b3dbfb13276e4bad4583ca81
mirkoCrobu <214636120+mirkoCrobu@users.noreply.github.com>
2026-08-03 12:33:44 +0200

    bump-runner-to-0.12.0-rc7 (#563)

--- internal/orchestrator/config/config.go
+++ internal/orchestrator/config/config.go
@@ -22,7 +22,7 @@ import (
 )
 
 // runnerVersion do not edit, this is generate with `task bump:runner-version`
-var RunnerVersion = "0.12.0rc6"
+var RunnerVersion = "0.12.0rc7"
 
 type Configuration struct {
 	appsDir                          *paths.Path
