e196a485f66187ca163128b5d18dd785b0ca96f4
dido18 <davideneri18@gmail.com>
2026-08-12 11:32:30 +0200

    feat: update README and implement Greeter class for custom Brick

--- internal/orchestrator/app/generator/testdata/app-with-brick.golden/bricks/my-brick/__init__.py
+++ internal/orchestrator/app/generator/testdata/app-with-brick.golden/bricks/my-brick/__init__.py
@@ -1,1 +1,20 @@
-# put your python code here
+from arduino.app_utils import brick, Logger
+import time
+
+logger = Logger("GreeterBrick")
+
+@brick
+class Greeter:
+    def __init__(self, name="World"):
+        self.name = name
+
+    def start(self):
+        logger.info("Starting Greeter")
+
+    def stop(self):
+        logger.info("Stopping Greeter")
+
+    # This is a non-blocking method that will be called repeatedly
+    def loop(self):
+        logger.info(f"Hello, {self.name}!")
+        time.sleep(1)
