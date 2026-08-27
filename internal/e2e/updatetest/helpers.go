53f4d3adc8c7f6cf98e812a80a435ef5b0e0b330
lucarin91 <l.rinaldi@arduino.cc>
2026-08-27 16:19:09 +0200

    remove another arduino user

--- internal/e2e/updatetest/helpers.go
+++ internal/e2e/updatetest/helpers.go
@@ -180,7 +180,7 @@ func getAppCliVersion(t *testing.T, containerName string) string {
 
 	cmd := exec.Command(
 		"docker", "exec",
-		"--user", "arduino",
+		"--user", "1000",
 		containerName,
 		"arduino-app-cli", "version", "--format", "json",
 	)
