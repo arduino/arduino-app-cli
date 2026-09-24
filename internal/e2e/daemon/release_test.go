5a00c3fa0d4167e644ae95cfcf0d4fce1e71c5c9
Marta Carbone <m.carbone@arduino.cc>
2026-09-24 17:48:39 +0200

    Fix test

--- internal/e2e/daemon/release_test.go
+++ internal/e2e/daemon/release_test.go
@@ -299,7 +299,7 @@ func TestAppReleasePrepare(t *testing.T) {
 				continue
 			}
 			t.Fatalf("install failed: code=%s message=%s", payload.Code, payload.Message)
-		case sseEventData:
+		case sseEventDone:
 			var payload struct {
 				ID string `json:"id"`
 			}
