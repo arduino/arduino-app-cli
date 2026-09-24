a31420393c90972c77b09b6986e9540cf041e67b
mirkoCrobu <214636120+mirkoCrobu@users.noreply.github.com>
2026-09-24 11:08:48 +0200

    bump bricks (#684)

--- internal/orchestrator/config/config.go
+++ internal/orchestrator/config/config.go
@@ -22,7 +22,7 @@ import (
 )
 
 // runnerVersion do not edit, this is generate with `task bump:runner-version`
-var RunnerVersion = "0.13.0rc2"
+var RunnerVersion = "0.13.0rc5"
 
 type Configuration struct {
 	appsDir                          *paths.Path
